09bfa3ff3c11b40843070d10cf370513afccf2d0
Dobby <dobby@DobbydeMacBook-Pro.local>
2018-03-03 04:34:32 +0800

    create and add simple code

---
+++ L1-Var&constant/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L1-Var&constant
+//
+//  Created by Dobby on 03/03/2018.
+//  Copyright © 2018 Dobby. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
